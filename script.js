58caf092a84ccf155914a0c3d0b2d210a349a7d7
RicardoAgk <71658779+RicardoAgk@users.noreply.github.com>
2022-01-12 18:49:30 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var nifInput = document.forms["form"]["nif"];
+
+function nifAlert() {
+    if (nifInput.value.length == !9) {
+        document.getElementsByClassName('nif-error-message')[0].style.visibility = "visible";
+    } else {
+        document.getElementsByClassName('nif-error-message')[0].style.visibility = "hidden";
+    }
+
+}
