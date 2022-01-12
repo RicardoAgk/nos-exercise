3bdd69293672aaa9798e2832b690d3d712722752
RicardoAgk <71658779+RicardoAgk@users.noreply.github.com>
2022-01-12 22:11:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 var nifInput = document.forms["form"]["nif"];
 
 function nifAlert() {
-    if (nifInput.value.length == !9) {
-        document.getElementsByClassName('nif-error-message')[0].style.visibility = "visible";
+    if(nifInput.value.length != 9) {     
+       document.getElementsByClassName('nif-error-message')[0].style.visibility = "visible"; 
     } else {
-        document.getElementsByClassName('nif-error-message')[0].style.visibility = "hidden";
+        document.getElementsByClassName('nif-error-message')[0].style.visibility = "hidden";  
     }
-
 }
